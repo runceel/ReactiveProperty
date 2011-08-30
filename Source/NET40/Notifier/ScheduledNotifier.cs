01fcc5b7330d65f08814704eab50dc1f57e339e3
neuecc <none@none>
2011-08-31 03:42:55 +0900

    Add ScheduledNotifier

---
+++ Source/NET40/Notifier/ScheduledNotifier.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Subjects;
+
+namespace Codeplex.Reactive.Notifier
+{
+    public class ScheduledNotifier<T> : IObservable<T>
+    {
+        readonly IScheduler scheduler;
+        readonly Subject<T> trigger = new Subject<T>();
+
+        public ScheduledNotifier(IScheduler scheduler)
+        {
+            if (scheduler == null) throw new ArgumentNullException("scheduler");
+
+            this.scheduler = scheduler;
+        }
+
+        public void Raise(T value)
+        {
+            scheduler.Schedule(() => trigger.OnNext(value));
+        }
+
+        public void Raise(T value, TimeSpan dueTime)
+        {
+            scheduler.Schedule(dueTime, () => trigger.OnNext(value));
+        }
+
+        public void Raise(T value, DateTimeOffset dueTime)
+        {
+            scheduler.Schedule(dueTime, () => trigger.OnNext(value));
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            return trigger.Subscribe(observer);
+        }
+    }
+}
---
+++ Test/ReactiveProperty.Tests/Notifier/ScheduledNotifierTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using Codeplex.Reactive.Notifier;
+using Microsoft.Reactive.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReactiveProperty.Tests
+{
+    [TestClass]
+    public class ScheduledNotifierTest : ReactiveTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var testScheduler = new TestScheduler();
+            var recorder = testScheduler.CreateObserver<int>();
+
+            var notifier = new ScheduledNotifier<int>(testScheduler);
+            notifier.Subscribe(recorder);
+
+            notifier.Raise(1);
+            notifier.Raise(2);
+            notifier.Raise(3, TimeSpan.FromMinutes(10));
+            notifier.Raise(4, TimeSpan.FromMinutes(1));
+            notifier.Raise(5, TimeSpan.FromMinutes(5));
+            notifier.Raise(6);
+
+            testScheduler.Start();
+
+            recorder.Messages.Is(
+                OnNext(1, 1),
+                OnNext(1, 2),
+                OnNext(1, 6),
+                OnNext(TimeSpan.FromMinutes(1).Ticks, 4),
+                OnNext(TimeSpan.FromMinutes(5).Ticks, 5),
+                OnNext(TimeSpan.FromMinutes(10).Ticks, 3));
+        }
+
+        [TestMethod]
+        public void Constructor()
+        {
+            var e = AssertEx.Throws<ArgumentNullException>(() => new ScheduledNotifier<int>(null));
+            e.ParamName.Is("scheduler");
+        }
+
+        [TestMethod]
+        public void Subscribe()
+        {
+            AssertEx.Throws<ArgumentNullException>(() => 
+                new ScheduledNotifier<int>(Scheduler.CurrentThread).Subscribe(null));
+        }
+    }
+}
