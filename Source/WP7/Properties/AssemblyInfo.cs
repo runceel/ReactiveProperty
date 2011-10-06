6d386a81fa59ef654983574e1911f7224a3702cd
neuecc <none@none>
2011-10-06 22:33:15 +0900

    ready to release reference

--- Source/WP7/Properties/AssemblyInfo.cs
+++ Source/WP7/Properties/AssemblyInfo.cs
@@ -3,35 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Resources;
 
-// アセンブリに関する一般情報は、以下の属性セットによって制御されます。 
-// アセンブリに関連付けられている情報を変更するには、
-// これらの属性値を変更します。
-[assembly: AssemblyTitle("ReactiveProperty.WP7")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ReactiveProperty.WP7")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから 
-// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、 
-// その型の ComVisible 属性を true に設定します。
-[assembly: ComVisible(false)]
-
-// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
 [assembly: Guid("1d98a326-401b-4a99-97ea-2ff3697c266f")]
-
-// アセンブリのバージョン情報は、以下の 4 つの値で構成されます。
-//
-//      メジャー バージョン
-//      マイナー バージョン
-//      ビルド番号
-//      リビジョン
-//
-// すべての値を指定するか、次のように '*' を使ってリビジョンおよびビルド番号を 
-// 既定値にすることができます。
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("ja-JP")]
