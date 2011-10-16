3b4af94f7067b621765320a2f3e171a5b6403c70
neuecc <none@none>
2011-10-17 03:27:06 +0900

    add Experimental Projects

---
+++ Experimental/NET45/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: Guid("20eef5be-44ca-4589-a56a-f4c0a97b54db")]
---
+++ Source/NET45/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: Guid("e7e2c09a-c4e3-4fba-be0a-804a93affda4")]
