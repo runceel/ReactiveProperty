67b74cf3bf844acacf014d502daad4f4bdb20615
neuecc <none@none>
2011-10-19 02:14:57 +0900

    fix SignalNotifierTest

--- Test/ReactiveProperty.Tests/Notifier/SignalNotifierTest.cs
+++ Test/ReactiveProperty.Tests/Notifier/SignalNotifierTest.cs
@@ -36,21 +36,46 @@ namespace ReactiveProperty.Tests
                 OnNext(0, SignalChangedStatus.Decrement),
                 OnNext(0, SignalChangedStatus.Empty));
 
+            // over max
             notifier.Increment(10);
             notifier.Count.Is(10);
-            AssertEx.Throws<InvalidOperationException>(() => notifier.Increment());
+            recorder.Messages.Clear();
+            notifier.Increment();
+            notifier.Count.Is(10);
+            recorder.Messages.Count.Is(0);
 
+            // under zero
             notifier.Decrement(10);
             notifier.Count.Is(0);
-            AssertEx.Throws<InvalidOperationException>(() => notifier.Decrement());
+            recorder.Messages.Clear();
+            notifier.Decrement();
+            notifier.Count.Is(0);
+            recorder.Messages.Count.Is(0);
 
+            // over inc
+            recorder.Messages.Clear();
             notifier.Increment(5);
-            notifier.Count.Is(5);
+            notifier.Increment(15);
+            notifier.Count.Is(10);
+            recorder.Messages.Is(
+                OnNext(0, SignalChangedStatus.Increment),
+                OnNext(0, SignalChangedStatus.Increment),
+                OnNext(0, SignalChangedStatus.Max));
+
+            // over dec
+            recorder.Messages.Clear();
+            notifier.Decrement(5);
+            notifier.Decrement(15);
+            notifier.Count.Is(0);
+            recorder.Messages.Is(
+                OnNext(0, SignalChangedStatus.Decrement),
+                OnNext(0, SignalChangedStatus.Decrement),
+                OnNext(0, SignalChangedStatus.Empty));
+
             AssertEx.Throws<ArgumentException>(() =>
                 notifier.Increment(-1));
             AssertEx.Throws<ArgumentException>(() =>
                 notifier.Decrement(-1));
-
             AssertEx.Throws<ArgumentException>(() =>
                 new SignalNotifier(0));
             AssertEx.DoesNotThrow(() =>
