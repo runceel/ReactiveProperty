a45d90e4d56b3b538cae2ed33c544a49f1b1b35d
neuecc <none@none>
2011-11-15 23:04:41 +0900

    Add OldNewPair ToString

--- Source/NET40/Extensions/OldNewPair.cs
+++ Source/NET40/Extensions/OldNewPair.cs
@@ -14,7 +14,7 @@
 
         public override string ToString()
         {
-            return "(" + OldItem + ", " + NewItem + ")";
+            return "{ Old = " + OldItem + ", New = " + NewItem + " }";
         }
     }
 }
---
+++ Test/ReactiveProperty.Tests/Extensions/OldNewPairTest.cs
@@ -0,0 +1,28 @@
+﻿using Codeplex.Reactive.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Codeplex.Reactive;
+using Microsoft.Reactive.Testing;
+using System.Linq;
+
+namespace ReactiveProperty.Tests
+{
+    [TestClass()]
+    public class OldNewPairTest
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            // ToString result same as AnonymousClass
+
+            new OldNewPair<string>("old", "new")
+                .ToString()
+                .Is(new { Old = "old", New = "new" }.ToString());
+
+            new OldNewPair<int>(10, 20)
+                .ToString()
+                .Is(new { Old = 10, New = 20 }.ToString());
+        }
+    }
+}
