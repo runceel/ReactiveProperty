3a3e4e4c5f90408c3e1f3cf5959b2e09fcee6231
neuecc <none@none>
2011-11-20 21:32:34 +0900

    0.3 published

--- BuildTools/push.bat
+++ BuildTools/push.bat
@@ -1,3 +1,3 @@
-nuget push ReactiveProperty.0.2.0.0.nupkg
-nuget push ReactiveProperty-Experimental.0.2.0.0.nupkg
-nuget push ReactiveProperty-WP7.0.2.0.0.nupkg
+nuget push ReactiveProperty.0.3.0.0.nupkg
+nuget push ReactiveProperty-Experimental.0.3.0.0.nupkg
+nuget push ReactiveProperty-WP7.0.3.0.0.nupkg
