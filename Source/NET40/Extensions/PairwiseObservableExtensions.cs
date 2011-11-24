5ae125a4a76b4cc43970f6d7e46d5e6e70284e13
neuecc <none@none>
2011-11-25 00:10:03 +0900

    Pairwise improved performance

--- Source/NET40/Extensions/PairwiseObservableExtensions.cs
+++ Source/NET40/Extensions/PairwiseObservableExtensions.cs
@@ -30,26 +30,34 @@ namespace Codeplex.Reactive.Extensions
             Contract.Requires<ArgumentNullException>(selector != null);
             Contract.Ensures(Contract.Result<IObservable<TR>>() != null);
 
-            var result = Observable.Defer<TR>(() =>
+            var result = Observable.Create<TR>(observer =>
             {
                 T prev = default(T);
                 var isFirst = true;
 
-                return source.Select(x =>
+                return source.Subscribe(x =>
                 {
                     if (isFirst)
                     {
                         isFirst = false;
                         prev = x;
-                        return default(TR);
+                        return;
                     }
-                    else
+
+                    TR value;
+                    try
                     {
-                        var value = selector(prev, x);
+                        value = selector(prev, x);
                         prev = x;
-                        return value;
                     }
-                }).Skip(1);
+                    catch (Exception ex)
+                    {
+                        observer.OnError(ex);
+                        return;
+                    }
+
+                    observer.OnNext(value);
+                }, observer.OnError, observer.OnCompleted);
             });
 
             Contract.Assume(result != null);
