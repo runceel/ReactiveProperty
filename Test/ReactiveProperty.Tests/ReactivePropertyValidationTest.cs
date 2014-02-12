db67dc404c4e80080dc5f14872c58d18bf5c7845
okazuki <k.ota.0130@gmail.com>
2014-02-13 00:14:33 +0900

    add Task validation test method

--- Test/ReactiveProperty.Tests/ReactivePropertyValidationTest.cs
+++ Test/ReactiveProperty.Tests/ReactivePropertyValidationTest.cs
@@ -5,6 +5,8 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Collections;
 
 namespace ReactiveProperty.Tests
 {
@@ -54,13 +56,100 @@ namespace ReactiveProperty.Tests
         [TestMethod]
         public void BothTest()
         {
+            var errors = new List<string>();
+            target.BothProperty
+                .ObserveErrorChanged
+                .OfType<string>()
+                .Subscribe(errors.Add);
+
             target.BothProperty.HasErrors.IsTrue();
+            errors.Count.Is(0);
 
             target.BothProperty.Value = "a";
             target.BothProperty.HasErrors.IsFalse();
+            errors.Count.Is(0);
 
             target.BothProperty.Value = "aaaaaa";
             target.BothProperty.HasErrors.IsTrue();
+            errors.Count.Is(1);
+        }
+
+        [TestMethod]
+        public void TaskTest()
+        {
+            var errors = new List<string>();
+            target.TaskValidationTestProperty
+                .ObserveErrorChanged
+                .OfType<string>()
+                .Subscribe(errors.Add);
+            errors.Count.Is(0);
+
+            target.TaskValidationTestProperty.Value = "a";
+            target.TaskValidationTestProperty.HasErrors.IsFalse();
+            errors.Count.Is(0);
+
+            target.TaskValidationTestProperty.Value = null;
+            target.TaskValidationTestProperty.HasErrors.IsTrue();
+            errors.Count.Is(1);
+        }
+
+        [TestMethod]
+        public async Task AsyncValidation_SuccessCase()
+        {
+            var source = new TaskCompletionSource<IEnumerable>();
+            var p = new ReactiveProperty<string>()
+                .SetValidateNotifyError(_ =>
+                {
+                    return source.Task;
+                });
+
+            var errors = new List<string>();
+            p.ObserveErrorChanged
+                .OfType<string>()
+                .Subscribe(errors.Add);
+
+            p.Value = "a"; // fire validation logic;
+            p.HasErrors.IsFalse();
+            errors.Count.Is(0);
+
+            source.SetResult(null); // validation success!
+
+            await Task.Run(() => 
+            {
+                p.HasErrors.IsFalse();
+                errors.Count.Is(0);
+            });
+        }
+
+        [TestMethod]
+        public async Task AsyncValidation_FailedCase()
+        {
+            var source = new TaskCompletionSource<IEnumerable>();
+            var p = new ReactiveProperty<string>()
+                .SetValidateNotifyError(_ =>
+                {
+                    return source.Task;
+                });
+
+            var errors = new List<string>();
+            p.ObserveErrorChanged
+                .OfType<string>()
+                .Subscribe(errors.Add);
+
+            p.Value = "a"; // fire validation logic;
+            p.HasErrors.IsFalse();
+            errors.Count.Is(0);
+
+            source.SetResult("error message"); // validation error!
+
+            await Task.Run(() => 
+            {
+                // check state
+                p.HasErrors.IsTrue();
+                errors.Count.Is(1, "error count is 1");
+                errors[0].Is("error message");
+                p.GetErrors("Value").Is("error message");
+            });
         }
 
     }
@@ -73,6 +162,8 @@ namespace ReactiveProperty.Tests
         [StringLength(5, ErrorMessage = "5over")]
         public ReactiveProperty<string> BothProperty { get; private set; }
 
+        public ReactiveProperty<string> TaskValidationTestProperty { get; private set; }
+
         public TestTarget()
         {
             this.RequiredProperty = new ReactiveProperty<string>()
@@ -81,6 +172,16 @@ namespace ReactiveProperty.Tests
             this.BothProperty = new ReactiveProperty<string>()
                 .SetValidateAttribute(() => BothProperty)
                 .SetValidateNotifyError(s => string.IsNullOrWhiteSpace(s) ? "required" : null);
+
+            this.TaskValidationTestProperty = new ReactiveProperty<string>()
+                .SetValidateNotifyError(async s =>
+                {
+                    if (string.IsNullOrWhiteSpace(s))
+                    {
+                        return await Task.FromResult("required");
+                    }
+                    return await Task.FromResult((string) null);
+                });
         }
     }
 }
