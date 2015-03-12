d9643568d783479bb27fce4d5470c04f13bf8b97
Kazuki <k.ota.0130@gmail.com>
2015-03-13 08:39:47 +0900

    add Comment

--- Source/ReactiveProperty.Portable-NET45+WINRT+WP8/ReadOnlyReactiveProperty.cs
+++ Source/ReactiveProperty.Portable-NET45+WINRT+WP8/ReadOnlyReactiveProperty.cs
@@ -1,19 +1,21 @@
 ﻿using Reactive.Bindings.Extensions;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Reactive.Concurrency;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Reactive.Bindings
 {
+    /// <summary>
+    /// Read only version ReactiveProperty.
+    /// </summary>
+    /// <typeparam name="T">Type of property.</typeparam>
     public class ReadOnlyReactiveProperty<T> : IObservable<T>, INotifyPropertyChanged, IDisposable
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         private Subject<T> source;
 
         private T latestValue;
@@ -48,6 +50,9 @@ namespace Reactive.Bindings
             connectable.Connect().AddTo(this.subscription);
         }
 
+        /// <summary>
+        /// Get latest value.
+        /// </summary>
         public T Value
         {
             get { return this.latestValue; }
@@ -58,8 +63,6 @@ namespace Reactive.Bindings
             return this.source.Subscribe(observer);
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
         public void Dispose()
         {
             if (!this.subscription.IsDisposed)
@@ -69,8 +72,20 @@ namespace Reactive.Bindings
         }
     }
 
+    /// <summary>
+    /// ReadOnlyReactiveProperty factory methods.
+    /// </summary>
     public static class ReadOnlyReactiveProperty
     {
+        /// <summary>
+        /// Create ReadOnlyReactiveProperty
+        /// </summary>
+        /// <typeparam name="T">Type of property.</typeparam>
+        /// <param name="self">source stream</param>
+        /// <param name="initialValue">initial push value</param>
+        /// <param name="mode">ReactivePropertyMode. Default is DistinctUntilChanged | RaiseLatestValueOnSubscribe</param>
+        /// <param name="eventScheduler">Scheduler of PropertyChanged event.</param>
+        /// <returns></returns>
         public static ReadOnlyReactiveProperty<T> ToReadOnlyReactiveProperty<T>(this IObservable<T> self,
             T initialValue = default(T),
             ReactivePropertyMode mode = ReactivePropertyMode.DistinctUntilChanged | ReactivePropertyMode.RaiseLatestValueOnSubscribe,
