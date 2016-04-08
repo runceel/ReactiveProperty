acd175d6c18bb8438183218eef5a6491a629391e
Kazuki <k.ota.0130@gmail.com>
2016-04-09 00:36:47 +0900

    add BusyNotifier

---
+++ Source/ReactiveProperty.Portable-NET45+WINRT+WP8/Notifiers/BusyNotifier.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Disposables;
+using System.Reactive.Subjects;
+
+namespace Reactive.Bindings.Notifiers
+{
+    /// <summary>
+    /// Notify of busy.
+    /// </summary>
+    public class BusyNotifier : INotifyPropertyChanged, IObservable<bool>
+    {
+
+        private static readonly PropertyChangedEventArgs IsBusyPropertyChangedEventArgs = new PropertyChangedEventArgs(nameof(IsBusy));
+
+        /// <summary>
+        /// property changed event handler
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int ProcessCounter { get; set; }
+
+        private Subject<bool> IsBusySubject { get; } = new Subject<bool>();
+
+        private object LockObject { get; } = new object();
+
+        private bool isBusy;
+
+        /// <summary>
+        /// Is process running.
+        /// </summary>
+        public bool IsBusy
+        {
+            get { return this.isBusy; }
+            set
+            {
+                if (this.isBusy == value) { return; }
+                this.isBusy = value;
+                this.PropertyChanged?.Invoke(this, IsBusyPropertyChangedEventArgs);
+                this.IsBusySubject.OnNext(this.isBusy);
+            }
+        }
+
+        /// <summary>
+        /// Process start.
+        /// </summary>
+        /// <returns>Call dispose method when process end.</returns>
+        public IDisposable ProcessStart()
+        {
+            lock (this.LockObject)
+            {
+                this.ProcessCounter++;
+                this.IsBusy = this.ProcessCounter != 0;
+                return Disposable.Create(() =>
+                {
+                    lock (this.LockObject)
+                    {
+                        this.ProcessCounter--;
+                        this.IsBusy = this.ProcessCounter != 0;
+                    }
+                });
+            }
+        }
+
+        /// <summary>
+        /// Subscribe busy.
+        /// </summary>
+        /// <param name="observer">observer</param>
+        /// <returns>disposable</returns>
+        public IDisposable Subscribe(IObserver<bool> observer)
+        {
+            observer.OnNext(this.IsBusy);
+            return this.IsBusySubject.Subscribe(observer);
+        }
+    }
+}
---
+++ Test/ReactiveProperty.Tests/Notifiers/BusyNotifierTest.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reactive.Bindings.Notifiers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveProperty.Tests.Notifiers
+{
+    [TestClass]
+    public class BusyNotifierTest
+    {
+        [TestMethod]
+        public void SimpleCase()
+        {
+            var n = new BusyNotifier();
+            var notifyPropertyChangedCounter = 0;
+            n.PropertyChanged += (_, e) =>
+            {
+                if (e.PropertyName == nameof(BusyNotifier.IsBusy))
+                {
+                    notifyPropertyChangedCounter++;
+                }
+            };
+            var observeNotifyIsBusyCounter = 0;
+            var latestNotifyIsBusyValue = false;
+            n.Subscribe(x =>
+            {
+                observeNotifyIsBusyCounter++;
+                latestNotifyIsBusyValue = x;
+            });
+
+            notifyPropertyChangedCounter.Is(0);
+            observeNotifyIsBusyCounter.Is(1);
+            latestNotifyIsBusyValue.IsFalse();
+
+            var d = n.ProcessStart();
+
+            notifyPropertyChangedCounter.Is(1);
+            observeNotifyIsBusyCounter.Is(2);
+            latestNotifyIsBusyValue.IsTrue();
+            n.IsBusy.IsTrue();
+
+            d.Dispose();
+
+            notifyPropertyChangedCounter.Is(2);
+            observeNotifyIsBusyCounter.Is(3);
+            latestNotifyIsBusyValue.IsFalse();
+            n.IsBusy.IsFalse();
+        }
+
+        [TestMethod]
+        public void MultipleCase()
+        {
+            var n = new BusyNotifier();
+            var notifyPropertyChangedCounter = 0;
+            n.PropertyChanged += (_, e) =>
+            {
+                if (e.PropertyName == nameof(BusyNotifier.IsBusy))
+                {
+                    notifyPropertyChangedCounter++;
+                }
+            };
+            var observeNotifyIsBusyCounter = 0;
+            var latestNotifyIsBusyValue = false;
+            n.Subscribe(x =>
+            {
+                observeNotifyIsBusyCounter++;
+                latestNotifyIsBusyValue = x;
+            });
+
+            notifyPropertyChangedCounter.Is(0);
+            observeNotifyIsBusyCounter.Is(1);
+            latestNotifyIsBusyValue.IsFalse();
+
+            var d1 = n.ProcessStart();
+            var d2 = n.ProcessStart();
+
+            notifyPropertyChangedCounter.Is(1);
+            observeNotifyIsBusyCounter.Is(2);
+            latestNotifyIsBusyValue.IsTrue();
+            n.IsBusy.IsTrue();
+
+            d1.Dispose();
+
+            notifyPropertyChangedCounter.Is(1);
+            observeNotifyIsBusyCounter.Is(2);
+            latestNotifyIsBusyValue.IsTrue();
+            n.IsBusy.IsTrue();
+
+            d2.Dispose();
+
+            notifyPropertyChangedCounter.Is(2);
+            observeNotifyIsBusyCounter.Is(3);
+            latestNotifyIsBusyValue.IsFalse();
+            n.IsBusy.IsFalse();
+        }
+
+        [TestMethod]
+        public void NotifyCase()
+        {
+            var n = new BusyNotifier();
+
+            var d = n.ProcessStart();
+
+            var observeNotifyIsBusyCounter = 0;
+            var latestNotifyIsBusyValue = false;
+            n.Subscribe(x =>
+            {
+                observeNotifyIsBusyCounter++;
+                latestNotifyIsBusyValue = x;
+            });
+
+            observeNotifyIsBusyCounter.Is(1);
+            latestNotifyIsBusyValue.IsTrue();
+            n.IsBusy.IsTrue();
+
+            d.Dispose();
+
+            observeNotifyIsBusyCounter.Is(2);
+            latestNotifyIsBusyValue.IsFalse();
+            n.IsBusy.IsFalse();
+        }
+    }
+}
