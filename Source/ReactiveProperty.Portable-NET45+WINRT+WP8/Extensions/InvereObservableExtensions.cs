c2a937941123e17c71f5335dda87c0674da4c665
Kazuki <k.ota.0130@gmail.com>
2016-04-09 11:37:32 +0900

    add Inverse

---
+++ Source/ReactiveProperty.Portable-NET45+WINRT+WP8/Extensions/InvereObservableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Reactive.Bindings.Extensions
+{
+    /// <summary>
+    /// IObservable of bool extension methods.
+    /// </summary>
+    public static class InvereObservableExtensions
+    {
+        /// <summary>
+        /// Inverse bool value.
+        /// </summary>
+        /// <param name="self">IObservable of bool</param>
+        /// <returns>Inverse IObservable of bool</returns>
+        public static IObservable<bool> Inverse(this IObservable<bool> self)
+        {
+            return self.Select(x => !x);
+        }
+    }
+}
---
+++ Test/ReactiveProperty.Tests/Extensions/InverseObservableExtensionsTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Reactive.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+using Reactive.Bindings.Extensions;
+
+namespace ReactiveProperty.Tests.Extensions
+{
+    [TestClass]
+    public class InverseObservableExtensionsTest : ReactiveTest
+    {
+        [TestMethod]
+        public void Inverse()
+        {
+            var s = new Subject<bool>();
+            var obs = new TestScheduler().CreateObserver<bool>();
+            s.Inverse().Subscribe(obs);
+            s.OnNext(true);
+            s.OnNext(false);
+
+            obs.Messages.Is(
+                OnNext(0, false),
+                OnNext(0, true));
+        }
+    }
+}
