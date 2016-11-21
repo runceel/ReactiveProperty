4123c48051da1884795a5554dbd01924ef1b9c37
Kazuki <k_ota28@hotmail.com>
2016-11-21 17:15:56 +0900

    v3.2.2 set assembly version

--- Source/ReactiveProperty.Portable/Properties/AssemblyInfo.cs
+++ Source/ReactiveProperty.Portable/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.CompilerServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.2.2.0")]
+[assembly: AssemblyFileVersion("3.2.2.0")]
