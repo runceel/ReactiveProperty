9cb9cad8415ec80b35bf1f512c7f736b3fecad0e
Kazuki Ota <kaota@microsoft.com>
2018-01-06 14:46:29 +0900

    change conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,7 @@
 from recommonmark.parser import CommonMarkParser
+
 source_parsers = {
-'.md': CommonMarkParser,
+    '.md': CommonMarkParser,
 }
+
 source_suffix = ['.rst', '.md']
-html_theme = 'sphinx_rtd_theme'
