ead3c14da659a4ca38dc4b9d2d20d5d9249aa168
Yoshifumi Kawai <ils@neue.cc>
2018-01-17 22:58:18 +0900

    Add ReactivePropertySlim, ReadOnlyReactivePropertySlim

---
+++ Source/ReactiveProperty.NETStandard/ReactivePropertySlim.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reactive.Disposables;
+using System.Threading;
+
+namespace Reactive.Bindings
+{
+    // This file includes ReactivePropertySlim and ReadOnlyReactivePropertySlim.
+
+    internal interface IObserverLinkedList<T>
+    {
+        void UnsubscribeNode(ObserverNode<T> node);
+    }
+
+    internal sealed class ObserverNode<T> : IObserver<T>, IDisposable
+    {
+        readonly IObserver<T> observer;
+        IObserverLinkedList<T> list;
+
+        public ObserverNode<T> Previous { get; internal set; }
+        public ObserverNode<T> Next { get; internal set; }
+
+        public ObserverNode(IObserverLinkedList<T> list, IObserver<T> observer)
+        {
+            this.list = list;
+            this.observer = observer;
+        }
+
+        public void OnNext(T value)
+        {
+            observer.OnNext(value);
+        }
+
+        public void OnError(Exception error)
+        {
+            observer.OnError(error);
+        }
+
+        public void OnCompleted()
+        {
+            observer.OnCompleted();
+        }
+
+        public void Dispose()
+        {
+            var sourceList = Interlocked.Exchange(ref list, null);
+            if (sourceList != null)
+            {
+                sourceList.UnsubscribeNode(this);
+                sourceList = null;
+            }
+        }
+    }
+
+    public class ReactivePropertySlim<T> : IReactiveProperty<T>, IReadOnlyReactiveProperty<T>, IObserverLinkedList<T>
+    {
+        const int IsDisposedFlagNumber = 1 << 9; // (reserve 0 ~ 8)
+
+        // minimize field count
+        T latestValue;
+        ReactivePropertyMode mode; // None = 0, DistinctUntilChanged = 1, RaiseLatestValueOnSubscribe = 2, Disposed = (1 << 9)
+        readonly EqualityComparer<T> equalityComparer;
+        ObserverNode<T> root;
+        ObserverNode<T> last;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public T Value
+        {
+            get
+            {
+                return latestValue;
+            }
+            set
+            {
+                if (IsDistinctUntilChanged && equalityComparer.Equals(latestValue, value))
+                {
+                    return;
+                }
+
+                // Note:can set null and can set after disposed.
+                this.latestValue = value;
+                if (!IsDisposed)
+                {
+                    OnNextAndRaiseValueChanged(ref value);
+                }
+            }
+        }
+
+        public bool IsDisposed => (int)mode == IsDisposedFlagNumber;
+
+        object IReactiveProperty.Value
+        {
+            get
+            {
+                return (object)Value;
+            }
+            set
+            {
+                Value = (T)value;
+            }
+        }
+
+        object IReadOnlyReactiveProperty.Value
+        {
+            get
+            {
+                return (object)Value;
+            }
+        }
+
+        bool IsDistinctUntilChanged => (mode & ReactivePropertyMode.DistinctUntilChanged) == ReactivePropertyMode.DistinctUntilChanged;
+        bool IsRaiseLatestValueOnSubscribe => (mode & ReactivePropertyMode.RaiseLatestValueOnSubscribe) == ReactivePropertyMode.RaiseLatestValueOnSubscribe;
+
+        public ReactivePropertySlim(T initialValue = default(T), ReactivePropertyMode mode = ReactivePropertyMode.DistinctUntilChanged | ReactivePropertyMode.RaiseLatestValueOnSubscribe, EqualityComparer<T> equalityComparer = null)
+        {
+            this.latestValue = initialValue;
+            this.mode = mode;
+            this.equalityComparer = equalityComparer ?? EqualityComparer<T>.Default;
+        }
+
+        void OnNextAndRaiseValueChanged(ref T value)
+        {
+            // call source.OnNext
+            var node = root;
+            while (node != null)
+            {
+                node.OnNext(value);
+                node = node.Next;
+            }
+
+            this.PropertyChanged?.Invoke(this, SingletonPropertyChangedEventArgs.Value);
+        }
+
+        public void ForceNotify()
+        {
+            OnNextAndRaiseValueChanged(ref latestValue);
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            if (IsDisposed)
+            {
+                observer.OnCompleted();
+                return Disposable.Empty;
+            }
+
+            if (IsRaiseLatestValueOnSubscribe)
+            {
+                observer.OnNext(this.latestValue);
+            }
+
+            // subscribe node, node as subscription.
+            var next = new ObserverNode<T>(this, observer);
+            if (root == null)
+            {
+                root = last = next;
+            }
+            else
+            {
+                last.Next = next;
+                next.Previous = last;
+                last = next;
+            }
+            return next;
+        }
+
+        void IObserverLinkedList<T>.UnsubscribeNode(ObserverNode<T> node)
+        {
+            if (node == root)
+            {
+                root = node.Next;
+            }
+            if (node == last)
+            {
+                last = node.Previous;
+            }
+
+            if (node.Previous != null)
+            {
+                node.Previous.Next = node.Next;
+            }
+            if (node.Next != null)
+            {
+                node.Next.Previous = node.Previous;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (IsDisposed) return;
+
+            var node = root;
+            root = last = null;
+            mode = (ReactivePropertyMode)IsDisposedFlagNumber;
+
+            while (node != null)
+            {
+                node.OnCompleted();
+                node = node.Next;
+            }
+        }
+
+        // NotSupported validation.
+
+        bool INotifyDataErrorInfo.HasErrors => throw new NotSupportedException();
+
+        IObservable<IEnumerable> IHasErrors.ObserveErrorChanged => throw new NotSupportedException();
+
+        IObservable<bool> IHasErrors.ObserveHasErrors => throw new NotSupportedException();
+
+        event EventHandler<DataErrorsChangedEventArgs> INotifyDataErrorInfo.ErrorsChanged
+        {
+            add
+            {
+                throw new NotSupportedException();
+            }
+
+            remove
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        IEnumerable INotifyDataErrorInfo.GetErrors(string propertyName)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public class ReadOnlyReactivePropertySlim<T> : IReadOnlyReactiveProperty<T>, IObserverLinkedList<T>, IObserver<T>
+    {
+        const int IsDisposedFlagNumber = 1 << 9; // (reserve 0 ~ 8)
+
+        // minimize field count
+        T latestValue;
+        IDisposable sourceSubscription;
+        ReactivePropertyMode mode; // None = 0, DistinctUntilChanged = 1, RaiseLatestValueOnSubscribe = 2, Disposed = (1 << 9)
+        readonly EqualityComparer<T> equalityComparer;
+
+        ObserverNode<T> root;
+        ObserverNode<T> last;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public T Value
+        {
+            get
+            {
+                return latestValue;
+            }
+        }
+
+        public bool IsDisposed => (int)mode == IsDisposedFlagNumber;
+
+        object IReadOnlyReactiveProperty.Value
+        {
+            get
+            {
+                return (object)Value;
+            }
+        }
+
+        bool IsDistinctUntilChanged => (mode & ReactivePropertyMode.DistinctUntilChanged) == ReactivePropertyMode.DistinctUntilChanged;
+        bool IsRaiseLatestValueOnSubscribe => (mode & ReactivePropertyMode.RaiseLatestValueOnSubscribe) == ReactivePropertyMode.RaiseLatestValueOnSubscribe;
+
+        public ReadOnlyReactivePropertySlim(IObservable<T> source, T initialValue = default(T), ReactivePropertyMode mode = ReactivePropertyMode.DistinctUntilChanged | ReactivePropertyMode.RaiseLatestValueOnSubscribe, EqualityComparer<T> equalityComparer = null)
+        {
+            this.latestValue = initialValue;
+            this.mode = mode;
+            this.equalityComparer = equalityComparer ?? EqualityComparer<T>.Default;
+            this.sourceSubscription = source.Subscribe(this);
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            if (IsDisposed)
+            {
+                observer.OnCompleted();
+                return Disposable.Empty;
+            }
+
+            if (IsRaiseLatestValueOnSubscribe)
+            {
+                observer.OnNext(latestValue);
+            }
+
+            // subscribe node, node as subscription.
+            var next = new ObserverNode<T>(this, observer);
+            if (root == null)
+            {
+                root = last = next;
+            }
+            else
+            {
+                last.Next = next;
+                next.Previous = last;
+                last = next;
+            }
+
+            return next;
+        }
+
+        void IObserverLinkedList<T>.UnsubscribeNode(ObserverNode<T> node)
+        {
+            if (node == root)
+            {
+                root = node.Next;
+            }
+            if (node == last)
+            {
+                last = node.Previous;
+            }
+
+            if (node.Previous != null)
+            {
+                node.Previous.Next = node.Next;
+            }
+            if (node.Next != null)
+            {
+                node.Next.Previous = node.Previous;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (IsDisposed) return;
+
+            var node = root;
+            root = last = null;
+            mode = (ReactivePropertyMode)IsDisposedFlagNumber;
+
+            while (node != null)
+            {
+                node.OnCompleted();
+                node = node.Next;
+            }
+            sourceSubscription.Dispose();
+            sourceSubscription = null;
+        }
+
+        void IObserver<T>.OnNext(T value)
+        {
+            if (IsDisposed) return;
+
+            if (IsDistinctUntilChanged && equalityComparer.Equals(latestValue, value))
+            {
+                return;
+            }
+
+            // SetValue
+            this.latestValue = value;
+
+            // call source.OnNext
+            var node = root;
+            while (node != null)
+            {
+                node.OnNext(value);
+                node = node.Next;
+            }
+
+            // Notify changed.
+            this.PropertyChanged?.Invoke(this, SingletonPropertyChangedEventArgs.Value);
+        }
+
+        void IObserver<T>.OnError(Exception error)
+        {
+            // do nothing.
+        }
+
+        void IObserver<T>.OnCompleted()
+        {
+            // oncompleted same as dispose.
+            Dispose();
+        }
+    }
+
+    public static class ReadOnlyReactivePropertySlim
+    {
+        public static ReadOnlyReactivePropertySlim<T> ToReadOnlyReactivePropertySlim<T>(this IObservable<T> source, T initialValue = default(T), ReactivePropertyMode mode = ReactivePropertyMode.DistinctUntilChanged | ReactivePropertyMode.RaiseLatestValueOnSubscribe, EqualityComparer<T> equalityComparer = null)
+        {
+            return new ReadOnlyReactivePropertySlim<T>(source, initialValue, mode, equalityComparer);
+        }
+    }
+}
---
+++ Test/ReactiveProperty.Tests/ReactivePropertySlimTest.cs
@@ -0,0 +1,120 @@
+﻿using Reactive.Bindings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using System.Reactive.Subjects;
+
+namespace ReactiveProperty.Tests
+{
+    [TestClass]
+    public class ReactivePropertySlimTest
+    {
+        [TestMethod]
+        public void NormalCase()
+        {
+            var rp = new ReactivePropertySlim<string>();
+            rp.Value.IsNull();
+            rp.Subscribe(x => x.IsNull());
+        }
+
+        [TestMethod]
+        public void InitialValue()
+        {
+            var rp = new ReactivePropertySlim<string>("Hello world");
+            rp.Value.Is("Hello world");
+            rp.Subscribe(x => x.Is("Hello world"));
+        }
+
+        [TestMethod]
+        public void NoRaiseLatestValueOnSubscribe()
+        {
+            var rp = new ReactivePropertySlim<string>(mode: ReactivePropertyMode.DistinctUntilChanged);
+            var called = false;
+            rp.Subscribe(_ => called = true);
+            called.Is(false);
+        }
+
+        [TestMethod]
+        public void NoDistinctUntilChanged()
+        {
+            var rp = new ReactivePropertySlim<string>(mode: ReactivePropertyMode.RaiseLatestValueOnSubscribe);
+            var list = new List<string>();
+            rp.Subscribe(list.Add);
+            rp.Value = "Hello world";
+            rp.Value = "Hello world";
+            rp.Value = "Hello japan";
+            list.Is(null, "Hello world", "Hello world", "Hello japan");
+        }
+
+        [TestMethod]
+        public void EnumCase()
+        {
+            var rp = new ReactivePropertySlim<TestEnum>();
+            var results = new List<TestEnum>();
+            rp.Subscribe(results.Add);
+            results.Is(TestEnum.None);
+
+            rp.Value = TestEnum.Enum1;
+            results.Is(TestEnum.None, TestEnum.Enum1);
+
+            rp.Value = TestEnum.Enum2;
+            results.Is(TestEnum.None, TestEnum.Enum1, TestEnum.Enum2);
+        }
+
+        [TestMethod]
+        public void ForceNotify()
+        {
+            var rp = new ReactivePropertySlim<int>(0);
+            var collecter = new List<int>();
+            rp.Subscribe(collecter.Add);
+
+            collecter.Is(0);
+            rp.ForceNotify();
+            collecter.Is(0, 0);
+        }
+
+        [TestMethod]
+        public void UnsubscribeTest()
+        {
+            var rp = new ReactivePropertySlim<int>(mode: ReactivePropertyMode.None);
+            var collecter = new List<(string, int)>();
+            var a = rp.Select(x => ("a", x)).Subscribe(collecter.Add);
+            var b = rp.Select(x => ("b", x)).Subscribe(collecter.Add);
+            var c = rp.Select(x => ("c", x)).Subscribe(collecter.Add);
+
+            rp.Value = 99;
+            collecter.Is(("a", 99), ("b", 99), ("c", 99));
+
+            collecter.Clear();
+            a.Dispose();
+
+            rp.Value = 40;
+            collecter.Is(("b", 40), ("c", 40));
+
+            collecter.Clear();
+            c.Dispose();
+
+            rp.Value = 50;
+            collecter.Is(("b", 50));
+
+            collecter.Clear();
+            b.Dispose();
+
+            rp.Value = 9999;
+            collecter.Count.Is(0);
+
+            var d = rp.Select(x => ("d", x)).Subscribe(collecter.Add);
+
+            rp.Value = 9;
+            collecter.Is(("d", 9));
+
+            rp.Dispose();
+        }
+    }
+}
