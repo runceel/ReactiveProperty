38e41f02d730e5e02bb66c299a400a65fe5d99da
Kazuki Ota <kaota@microsoft.com>
2018-08-02 14:25:12 +0900

    add test cases

--- Test/ReactiveProperty.Tests/AwaiterTest.cs
+++ Test/ReactiveProperty.Tests/AwaiterTest.cs
@@ -45,22 +45,18 @@ namespace ReactiveProperty.Tests
             IReactiveProperty<int> prop = new ReactiveProperty<int>();
             prop.Value = 999;
 
+            var tasks = new ConcurrentQueue<Task>();
             var values = new ConcurrentQueue<int>();
-            var rpAwaitTasks = new ConcurrentQueue<Task>();
-            var tasks = Enumerable.Range(1, 3)
-                .Select(x => Task.Run(new Action(async () =>
-                {
-                    var tcs = new TaskCompletionSource<int>();
-                    rpAwaitTasks.Enqueue(tcs.Task);
-                    values.Enqueue(await prop);
-                    tcs.SetResult(0);
-                })));
-            await Task.WhenAll(tasks);
-
+            Parallel.For(0, 3, async _ =>
+            {
+                var tcs = new TaskCompletionSource<int>();
+                tasks.Enqueue(tcs.Task);
+                values.Enqueue(await prop);
+                tcs.SetResult(0);
+            });
             prop.Value = 1000;
             prop.Value = 1001;
-
-            await Task.WhenAll(rpAwaitTasks);
+            await Task.WhenAll(tasks);
             values.Is(1000, 1000, 1000);
         }
 
@@ -70,22 +66,18 @@ namespace ReactiveProperty.Tests
             var cmd = new ReactiveCommand<int>();
             cmd.Execute(999);
 
+            var tasks = new ConcurrentQueue<Task>();
             var values = new ConcurrentQueue<int>();
-            var cmdAwaitTasks = new ConcurrentQueue<Task>();
-            var tasks = Enumerable.Range(1, 3)
-                .Select(x => Task.Run(new Action(async () =>
-                {
-                    var tcs = new TaskCompletionSource<int>();
-                    cmdAwaitTasks.Enqueue(tcs.Task);
-                    values.Enqueue(await cmd);
-                    tcs.SetResult(0);
-                })));
-            await Task.WhenAll(tasks);
-
+            Parallel.For(0, 3, async _ =>
+            {
+                var tcs = new TaskCompletionSource<int>();
+                tasks.Enqueue(tcs.Task);
+                values.Enqueue(await cmd);
+                tcs.SetResult(0);
+            });
             cmd.Execute(1000);
             cmd.Execute(1001);
-
-            await Task.WhenAll(cmdAwaitTasks);
+            await Task.WhenAll(tasks);
             values.Is(1000, 1000, 1000);
         }
     }
