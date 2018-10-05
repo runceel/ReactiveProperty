69484cc6a567716de78bf5f7ebe62893ba21be4e
Chris Pulman <chris.pulman@yahoo.com>
2018-10-05 17:46:46 +0100

    UWP Project Update Added comments to all public members Formated code

--- Source/ReactiveProperty.Platform.UWP/Interactivity/Behavior.cs
+++ Source/ReactiveProperty.Platform.UWP/Interactivity/Behavior.cs
@@ -2,13 +2,25 @@
 
 namespace Reactive.Bindings.Interactivity
 {
+    /// <summary>
+    /// Behavior
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <seealso cref="Reactive.Bindings.Interactivity.Behavior"/>
     public abstract class Behavior<T> : Behavior
         where T : DependencyObject
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Behavior{T}"/> class.
+        /// </summary>
         protected Behavior() : base(typeof(T))
         {
         }
 
+        /// <summary>
+        /// Gets the associated object.
+        /// </summary>
+        /// <value>The associated object.</value>
         protected new T AssociatedObject
         {
             get { return (T)base.AssociatedObject; }
