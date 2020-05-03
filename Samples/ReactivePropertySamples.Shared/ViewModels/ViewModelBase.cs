f263bb0c843ef6f08a3a317bc434eda6c5c3959a
Kazuki Ota <kaota@microsoft.com>
2020-05-03 20:58:37 +0900

    add samples

---
+++ Samples/ReactivePropertySamples.Shared/ViewModels/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reactive.Disposables;
+using System.Text;
+
+namespace ReactivePropertySamples.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        protected CompositeDisposable Disposables { get; } = new CompositeDisposable();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Dispose() => Disposables.Dispose();
+    }
+}
