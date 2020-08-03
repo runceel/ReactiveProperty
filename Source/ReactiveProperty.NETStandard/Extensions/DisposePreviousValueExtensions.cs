61d6b8f9acc7c2d3e750bc9b6d17af560489b2cb
Kazuki Ota <kaota@microsoft.com>
2020-08-03 14:15:14 +0900

    add DisposePreviousValue extension method

---
+++ Source/ReactiveProperty.NETStandard/Extensions/DisposePreviousValueExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace Reactive.Bindings.Extensions
+{
+    /// <summary>
+    /// DisposePreviousValue Ovservable Extensions
+    /// </summary>
+    public static class DisposePreviousValueExtensions
+    {
+        /// <summary>
+        /// Dispose previous value automatically.
+        /// </summary>
+        public static IObservable<T> DisposePreviousValue<T>(this IObservable<T> source) => 
+            Observable.Create<T>(ox =>
+            {
+                var d = new SerialDisposable();
+                return new CompositeDisposable(
+                    source.Do(x => d.Disposable = x as IDisposable).Do(ox).Subscribe(), 
+                    d);
+            });
+    }
+}
