1f117b6625870e8a10a4b27dd3efe913ce9698fc
Kazuki Ota <kaota@microsoft.com>
2021-05-31 14:54:03 +0900

    Update samples

--- Samples/ReactivePropertySamples.Shared/ViewModels/EventToReactiveViewModel.cs
+++ Samples/ReactivePropertySamples.Shared/ViewModels/EventToReactiveViewModel.cs
@@ -10,15 +10,18 @@ namespace ReactivePropertySamples.ViewModels
     public class EventToReactiveViewModel : ViewModelBase
     {
         public ReactivePropertySlim<string> MousePosition { get; }
-
+        public ReactivePropertySlim<bool> CanExecute { get; }
         public ReactiveCommand<string> OpenFileCommand { get; }
         public ReadOnlyReactivePropertySlim<string> OpenedFile { get; }
 
         public EventToReactiveViewModel()
         {
-            MousePosition = new ReactivePropertySlim<string>();
-
-            OpenFileCommand = new ReactiveCommand<string>();
+            MousePosition = new ReactivePropertySlim<string>()
+                .AddTo(Disposables);
+            CanExecute = new ReactivePropertySlim<bool>(true)
+                .AddTo(Disposables);
+            OpenFileCommand = CanExecute.ToReactiveCommand<string>()
+                .AddTo(Disposables);
             OpenedFile = OpenFileCommand.Select(x => $"You selected {x}")
                 .ToReadOnlyReactivePropertySlim()
                 .AddTo(Disposables);
