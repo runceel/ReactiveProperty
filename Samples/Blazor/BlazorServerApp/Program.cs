6cf3f8b99691b45ee5956cd1b7009ca07284c616
Kazuki Ota <k_ota28@hotmail.com>
2022-04-28 21:57:51 +0900

    add ReactivePropertiesValidator sample for Blazor server

--- Samples/Blazor/BlazorServerApp/Program.cs
+++ Samples/Blazor/BlazorServerApp/Program.cs
@@ -1,3 +1,4 @@
+﻿using BlazorSample.Shared.ViewModels;
 using BlazorServerApp.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddTransient<IndexViewModel>();
 
 var app = builder.Build();
 
