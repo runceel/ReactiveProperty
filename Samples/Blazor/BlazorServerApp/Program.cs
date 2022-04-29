45af63627b1d332d2108a249ab5ef152633f4b90
Kazuki Ota <k_ota28@hotmail.com>
2022-04-29 23:23:22 +0900

    update a sample for blazer

--- Samples/Blazor/BlazorServerApp/Program.cs
+++ Samples/Blazor/BlazorServerApp/Program.cs
@@ -9,7 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddTransient<IndexViewModel>();
+builder.Services.AddTransient<ValidationViewModel>();
+builder.Services.AddTransient<HelloWorldViewModel>();
 
 var app = builder.Build();
 
