ef1ab305f11b5b8e4e735934885d80c258a3bf0b
Kazuki Ota <kaota@microsoft.com>
2023-01-05 22:48:51 +0900

    add a few benchmarks

--- Benchmark/Benchmark.Current/BasicUsages.v9.cs
+++ Benchmark/Benchmark.Current/BasicUsages.v9.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Reactive.Subjects;
 using BenchmarkDotNet.Attributes;
 using Reactive.Bindings;
@@ -62,10 +63,75 @@ public partial class BasicUsages
     }
 
     [Benchmark]
+    public ReactivePropertyVM ReactivePropertyValidationFromPoco()
+    {
+        var vm = new ReactivePropertyVM();
+        vm.Name.Value = "valid";
+        vm.Name.Value = "";
+        vm.Dispose();
+        return vm;
+    }
+
+    [Benchmark]
+    public ValidatableReactivePropertyVM ValidatableReactivePropertyValidationFromPoco()
+    {
+        var vm = new ValidatableReactivePropertyVM();
+        vm.Name.Value = "valid";
+        vm.Name.Value = "";
+        vm.Dispose();
+        return vm;
+    }
+
+    [Benchmark]
     public IObservable<string> ObservePropertyLegacy()
     {
         var p = new Person();
         return p.ObservePropertyLegacy(x => x.Name);
     }
 
+    [Benchmark]
+    public ReactivePropertySlim<string> ToReactivePropertyAsSynchronizedSlim()
+    {
+        var p = new Person();
+        return p.ToReactivePropertySlimAsSynchronized(x => x.Name);
+    }
+}
+
+public class ReactivePropertyVM : IDisposable
+{
+    private Person _model = new();
+
+    [Required]
+    public ReactiveProperty<string> Name { get; }
+
+    public ReactivePropertyVM()
+    {
+        Name = _model.ToReactivePropertyAsSynchronized(x => x.Name,
+            ignoreValidationErrorValue: true)
+            .SetValidateAttribute(() => Name);
+    }
+
+    public void Dispose()
+    {
+        Name.Dispose();
+    }
+}
+
+public class ValidatableReactivePropertyVM : IDisposable
+{
+    private Person _model = new();
+
+    [Required]
+    public ValidatableReactiveProperty<string> Name { get; }
+
+    public ValidatableReactivePropertyVM()
+    {
+        Name = _model.ToReactivePropertySlimAsSynchronized(x => x.Name)
+            .ToValidatableReactiveProperty(() => Name, disposeSource: true);
+    }
+
+    public void Dispose()
+    {
+        Name.Dispose();
+    }
 }
