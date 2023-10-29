9c4aedc34cf3ca8d5ba8a610b0017ea7505e5053
Kazuki Ota <kaota@microsoft.com>
2023-10-29 14:45:36 +0900

    Release v9.3.4-pre1 (#456)
    * Update dotnet-core-unit-testing.yml
    
    * ReactivePropertyWpfScheduler throws StackOverflowException. (#436)
    
    * dispatch to ImmediateScheduler
    * Incrment version number
    
    * Update dotnet-core-unit-testing.yml
    
    * Remove set-output from workflow (#438)
    
    * Performance improvement for filtered collection (#441)
    
    * Remove LINQ
    
    * increment version number
    
    * detach PropertyChanged events (#445)
    
    * v9.3.3 (#446)
    
    * Bump @babel/traverse from 7.11.0 to 7.23.2 in /docs (#448)
    
    Bumps [@babel/traverse](https://github.com/babel/babel/tree/HEAD/packages/babel-traverse) from 7.11.0 to 7.23.2.
    - [Release notes](https://github.com/babel/babel/releases)
    - [Changelog](https://github.com/babel/babel/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/babel/babel/commits/v7.23.2/packages/babel-traverse)
    
    ---
    updated-dependencies:
    - dependency-name: "@babel/traverse"
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump browserify-sign from 4.2.1 to 4.2.2 in /docs (#451)
    
    Bumps [browserify-sign](https://github.com/crypto-browserify/browserify-sign) from 4.2.1 to 4.2.2.
    - [Changelog](https://github.com/browserify/browserify-sign/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/crypto-browserify/browserify-sign/compare/v4.2.1...v4.2.2)
    
    ---
    updated-dependencies:
    - dependency-name: browserify-sign
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * modified it to detach the event from Source.CollectionChanged (#449)
    
    * Update build-and-publish.yml (#454)
    
    * Fix #452 (#453)
    
    * v9.3.4
    
    * v9.3.4 (#455)
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Takaaki Suzuki <xin9le@live.jp>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: ryo1988 <6487424+ryo1988@users.noreply.github.com>

---
+++ Test/ReactiveProperty.WPF.ManualTests/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Reactive.Bindings;
+using Reactive.Bindings.Schedulers;
+
+namespace ReactiveProperty.WPF.ManualTests;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        ReactivePropertyScheduler.SetDefault(new ReactivePropertyWpfScheduler(Dispatcher));
+    }
+}
