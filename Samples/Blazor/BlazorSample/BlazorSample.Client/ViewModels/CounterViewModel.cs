562bbb0a6c4878dd34be9d2a5d1ab12fa49c31f9
Kazuki Ota <kaota@microsoft.com>
2023-11-18 12:04:35 +0900

    update blazor sample to .NET 8. Form validation on SSR doesn't work.

---
+++ Samples/Blazor/BlazorSample/BlazorSample.Client/ViewModels/CounterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Reactive.Disposables;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using Reactive.Bindings.TinyLinq;
+
+namespace BlazorSample.Client.ViewModels;
+public class CounterViewModel : IDisposable
+{
+    private CompositeDisposable _disposables = new();
+    public ReactivePropertySlim<int> Counter { get; }
+
+    public ReactiveCommandSlim IncrementCommand { get; }
+
+    public CounterViewModel()
+    {
+        Counter = new ReactivePropertySlim<int>(0)
+            .AddTo(_disposables);
+
+        IncrementCommand = Counter.Select(x => x < 10)
+            .ToReactiveCommandSlim()
+            .WithSubscribe(Increment, _disposables.Add)
+            .AddTo(_disposables);
+    }
+
+    private void Increment()
+    {
+        Counter.Value++;
+    }
+
+    public void Dispose() => _disposables.Dispose();
+}
